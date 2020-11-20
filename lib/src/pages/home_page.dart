3b7462321e19538aa95c198efd2c48a6fb2b2ea9
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-11-20 09:53:12 -0400

    agregamos iconos desde json

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,8 @@
 //import 'dart:js';
 
+import 'package:componentes_flutter/src/pages/alert_page.dart';
 import 'package:componentes_flutter/src/providers/menu_provider.dart';
+import 'package:componentes_flutter/src/utils/icono_string_utils.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -27,7 +29,7 @@ class HomePage extends StatelessWidget {
         initialData: [], // argumento opcional,
         // es algo que nos permite dibujar algo en la pantalla de nuestro despositivo
         builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-          return ListView(children: _listaItem(snapshot.data));
+          return ListView(children: _listaItem(snapshot.data, context));
         });
 
     /*return ListView(
@@ -35,14 +37,19 @@ class HomePage extends StatelessWidget {
     );*/
   }
 
-  List<Widget> _listaItem(List<dynamic> data) {
+  List<Widget> _listaItem(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
     data.forEach((opt) {
       final widgetTemp = ListTile(
         title: Text(opt['texto']),
-        leading: Icon(Icons.account_circle, color: Colors.blue),
+        leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) {
+            return AlertPage();
+          });
+          Navigator.push(context, route);
+        },
       );
       opciones..add(widgetTemp)..add(Divider());
     });
