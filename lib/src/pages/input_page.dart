f8ed49980360406303d4261efae2cffad1d89150
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-11-24 15:01:48 -0400

    finalizamos seccion de input clase 79

---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String _nombre;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inputs Titulo'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+        children: <Widget>[
+          _crearInputs(),
+          Divider(),
+          _crearPersona(),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearInputs() {
+    return TextField(
+      //autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        counter: Text('Letra ${_nombre}'),
+        hintText: 'Nombre de la persona',
+        labelText: 'Nombre prueba',
+        helperText: 'Solo es el nombre',
+        suffix: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle),
+      ),
+      onChanged: (valor) {
+        setState(() {
+          _nombre = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearPersona() {
+    return ListTile(
+      title: Text('Nombre es $_nombre'),
+    );
+  }
+}
